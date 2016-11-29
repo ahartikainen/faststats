a0941d868d0e2a328a5ff9d67581ae1ec2cbbea7
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2016-11-29 17:56:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
-	#!/usr/bin/env python
+#!/usr/bin/env python
 
-	from distutils.core import setup, Extension
-	import numpy.distutils.misc_util
+from distutils.core import setup, Extension
+import numpy.distutils.misc_util
 
+py_modules = []
 
-	py_modules = []
-
-		setup(
-			name="faststats",
-			version='0.0dev',
-			author="Morgan Fouesneau",
-			author_email="mfouesn@uw.edu",
-			py_modules=py_modules,
-			description="",
-			long_description=open("README.md").read(),
-			classifiers=[
-				"Development Status :: 0 - Beta",
-				"Intended Audience :: Science/Research",
-				"Operating System :: OS Independent",
-				"Programming Language :: Python",
-			    ],
-		    )
-
-		# python setup.py build_ext --inplace
+setup(
+	name="faststats",
+	version='0.0dev',
+	author="Morgan Fouesneau",
+	author_email="mfouesn@uw.edu",
+	py_modules=py_modules,
+	description="",
+	long_description=open("README.md").read(),
+	classifiers=[
+		"Development Status :: 0 - Beta",
+		"Intended Audience :: Science/Research",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+	            ],
+      )
+# python setup.py build_ext --inplace
